5651541559cc70694413c08ee58d79a21481e891
c C <cc@cs-Mac-Studio.local>
2023-10-20 16:36:00 +0800

    集成gin

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello 世界")
+	// 初始化 Gin 实例
+	r := gin.Default()
+
+	// 注册一个路由
+	r.GET("/", func(c *gin.Context) {
+
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	//处理404请求
+	r.NoRoute(func(ctx *gin.Context) {
+		//获取标头的Accept信息
+		acceptString := ctx.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			ctx.String(http.StatusNotFound, "页面返回404")
+		} else {
+			// 默认返回 JSON
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+
+	// 运行服务
+	r.Run(":8000")
 }
