ca50eb907d819f5ad19ec67c98b66e86258ff575
c C <cc@cs-Mac-Studio.local>
2023-10-20 16:21:17 +0800

    自动加载

--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:tniub.cc@gmail.com\">tniub.cc@gmail.com</a>")
-	} else {
-		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
-			"<p>如有疑惑，请联系我们。</p>")
-	}
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("hello 世界")
 }
