7f0776deae3f7ae5736ff22ee0844899bed2b461
lemon <tniub.cc@gmail.com>
2023-10-20 23:34:07 +0800

    response包

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -5,7 +5,7 @@ import (
 	v1 "cchub/app/http/controllers/api/v1"
 	"cchub/app/models/user"
 	"cchub/app/requests"
-	"net/http"
+	"cchub/pkg/response"
 
 	"github.com/gin-gonic/gin"
 )
@@ -25,7 +25,7 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 	}
 
 	//  检查数据库并返回响应
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
 }
@@ -36,7 +36,7 @@ func (sc *SignupController) IsEmailExist(c *gin.Context) {
 	if ok := requests.Validate(c, &request, requests.SignupEmailExist); !ok {
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
+	response.JSON(c, gin.H{
 		"exist": user.IsEmailExist(request.Email),
 	})
 }
