49a6e110184470c3707de83ac6aa1d7b4208f035
lemon <tniub.cc@gmail.com>
2023-10-20 18:18:49 +0800

    http访问日志

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"cchub/app/http/middlewares"
 	"cchub/routes"
 	"net/http"
 	"strings"
@@ -20,7 +21,7 @@ func SetupRoute(router *gin.Engine) {
 
 func registerGlobalMiddleWare(router *gin.Engine) {
 	router.Use(
-		gin.Logger(),
+		middlewares.Logger(),
 		gin.Recovery(),
 	)
 }
