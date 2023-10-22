6a0842746c0e1b58adf01fac019056e7f4d56e7c
lemon <tniub.cc@gmail.com>
2023-10-22 16:00:19 +0800

    刷新token

--- routes/api.go
+++ routes/api.go
@@ -38,6 +38,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			authGroup.POST("/login/using-phone", lgc.LoginByPhone)
 			// 支持手机号，Email 和 用户名
 			authGroup.POST("/login/using-password", lgc.LoginByPassword)
+			//刷新token
+			authGroup.POST("/login/refresh-token", lgc.RefreshToken)
 		}
 	}
 }
