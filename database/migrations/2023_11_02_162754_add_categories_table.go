0183cfee5da233d9a41d2261d41bb7e4c6209d49
lemon <tniub.cc@gmail.com>
2023-11-02 16:29:27 +0800

    分类模型

---
+++ database/migrations/2023_11_02_162754_add_categories_table.go
@@ -0,0 +1,31 @@
+package migrations
+
+import (
+	"cchub/app/models"
+	"cchub/pkg/migrate"
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Category struct {
+		models.BaseModel
+
+		Name        string `gorm:"type:varchar(255);not null;index"`
+		Description string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Category{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Category{})
+	}
+
+	migrate.Add("2023_11_02_162754_add_categories_table", up, down)
+}
